59507e9c2e775e875539ed9cae6c70e9497af4d7
Anton Heestand <anton.heestand@gmail.com>
2022-07-27 13:35:13 +0200

    Initial Commit

---
+++ Linker/LinkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkerApp.swift
+//  Linker
+//
+//  Created by Anton Heestand on 2022-07-27.
+//
+
+import SwiftUI
+
+@main
+struct LinkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
