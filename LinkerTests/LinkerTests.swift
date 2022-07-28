e73c43a8e8cc096b5e8cbc3dc9800458882ec458
Anton Heestand <anton.heestand@gmail.com>
2022-07-28 12:39:16 +0200

    Adds Tests

--- LinkerTests/LinkerTests.swift
+++ LinkerTests/LinkerTests.swift
@@ -11,26 +11,24 @@ import XCTest
 class LinkerTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testSignUp() async throws {
+        
+        let authServiceManager: AuthServiceManager = .mocked
+        
+        try await authServiceManager.signUp(email: "test@test.com", password: "123Abc!")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSignIn() async throws {
+        
+        let authServiceManager: AuthServiceManager = .mocked
+        
+        try await authServiceManager.signIn(email: "test@test.com", password: "123Abc!")
     }
-
 }
